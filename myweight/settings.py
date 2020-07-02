05ea3de8abf9cfa2bed002d20f11997b1b092731
varfgehu <varfalvigergo@gmail.com>
2020-07-02 11:29:21 +0100

    Updated settings

--- myweight/settings.py
+++ myweight/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0aax*6qxobd5qu9kx2f_2#@64ozgi)dba#d$gb72dm)pt_zdxo'
+# SECRET_KEY = '0aax*6qxobd5qu9kx2f_2#@64ozgi)dba#d$gb72dm)pt_zdxo'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mydjangoweight.herokuapp.com']
 
 
 # Application definition
